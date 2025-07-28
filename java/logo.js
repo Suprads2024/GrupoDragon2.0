c6036731419d60ec79c772ca0c165eeb8b896731
ignaciosoraka <ignaciosoraka@gmail.com>
2025-07-28 16:28:01 -0300

    arreglos

--- java/logo.js
+++ java/logo.js
@@ -7,11 +7,3 @@ window.addEventListener('scroll', function() {
     }
 });
 
-window.addEventListener('scroll', function() {
-    const logoImg = document.getElementById('logo-img');
-    if (window.scrollY > 50) {  // Ajusta este valor según el momento en que quieras que se cambie el logo
-        logoImg.src = '../images/Logo/Grupo Dragon_ Logo Negro.png'; // Ruta de la imagen negra
-    } else {
-        logoImg.src = '../images/Logo/Grupo Dragon_ Logo Blanco.png'; // Ruta de la imagen blanca
-    }
-});
