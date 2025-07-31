9ac52e1c015575fcf927f0d6157e7c8e521a5b04
ignaciosoraka <ignaciosoraka@gmail.com>
2025-07-31 14:08:38 -0300

    arreglos

--- java/nav.js
+++ java/nav.js
@@ -4,16 +4,20 @@ const toggleMenu = () => {
   const src = burgerMenu.getAttribute("src");
 
   // Detectar si estamos en la raíz o en una subcarpeta
-  const isRoot = location.pathname === "index.html" || location.pathname === "/";
+  const isRoot = location.pathname === "/" || location.pathname === "/index.html";
 
+  // Rutas según ubicación
   const burgerPath = isRoot ? "images/burger-menu.svg" : "../images/burger-menu.svg";
   const closePath = isRoot ? "images/close.svg" : "../images/close.svg";
 
-  const isBurger = src === burgerPath;
+  // Verificamos si está el ícono de burger o el de close
+  const isBurger = src.includes("burger-menu.svg");
   const iconName = isBurger ? closePath : burgerPath;
 
+  // Cambiar imagen
   burgerMenu.setAttribute("src", iconName);
 
+  // Animación del menú
   if (!isBurger) {
     navigation.classList.add("navigation--mobile--fadeout");
     setTimeout(() => {
